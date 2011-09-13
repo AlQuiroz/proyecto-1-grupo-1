6684f222b7db7f1d1969fe89dcf237b271c6880c
mar11176@uvg.edu.gt <mar11176@uvg.edu.gt@24f3b580-239b-6204-ef0e-72e19894f2bd>
2011-09-13 00:07:12 +0000

    Clase del administrativo

---
+++ Administrativo.java
@@ -0,0 +1,15 @@
+package proyecto1;
+
+public class Administrativo {
+	private String nombreAdministrador;
+	public Administrativo(){
+		nombreAdministrador="Pedro Lopez";
+	}
+	public String getNombreAdministrador() {
+		return nombreAdministrador;
+	}
+	public void setNombreAdministrador(String nombreAdministrador) {
+		this.nombreAdministrador = nombreAdministrador;
+	}
+
+}
