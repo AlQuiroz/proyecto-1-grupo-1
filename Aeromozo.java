c33202a6525c4b22480c96237ef5dfa0444c8c76
mar11176@uvg.edu.gt <mar11176@uvg.edu.gt@24f3b580-239b-6204-ef0e-72e19894f2bd>
2011-09-13 00:07:52 +0000

    Clase del aeromozo

---
+++ Aeromozo.java
@@ -0,0 +1,15 @@
+package proyecto1;
+
+public class Aeromozo {
+	private String nombreMozo;
+	public Aeromozo(){
+		nombreMozo = "María Castillo";
+	}
+	public String getNombreMozo() {
+		return nombreMozo;
+	}
+	public void setNombreMozo(String nombreMozo) {
+		this.nombreMozo = nombreMozo;
+	}
+	
+}
