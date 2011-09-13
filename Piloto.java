2f1a7e89bc72c6e7a7d1ccb88893904f150c68f6
mar11176@uvg.edu.gt <mar11176@uvg.edu.gt@24f3b580-239b-6204-ef0e-72e19894f2bd>
2011-09-13 00:10:59 +0000

    Clase Piloto

---
+++ Piloto.java
@@ -0,0 +1,15 @@
+package proyecto1;
+
+public class Piloto {
+	private String nombrePiloto;
+
+	public Piloto(){
+		nombrePiloto="Estuado Perez";
+	}
+	public String getNombrePiloto() {
+		return nombrePiloto;
+	}
+	public void setNombrePiloto(String nombrePiloto) {
+		this.nombrePiloto = nombrePiloto;
+	}
+}
